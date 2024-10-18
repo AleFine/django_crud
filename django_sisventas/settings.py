72201211b95d6e07876e9f8b86eccb2aaaa43033
Kevin <keving.kpg@gmail.com>
2024-10-18 15:29:36 -0500

    changes 3

--- django_sisventas/settings.py
+++ django_sisventas/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'django_sisventas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djventas',
+        'NAME': 'dbventitas',
         'USER':'root',
-        'PASSWORD':'root',
+        'PASSWORD':'',
         'LOCALHOST':'localhost',
         'PORT':'3306'
     }
